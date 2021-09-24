e7a63784e2ab4f6626fe417201b46d3385e0d6b8
nurulhodanahid11 <61221153+nurulhodanahid11@users.noreply.github.com>
2021-09-24 12:04:09 +0600

    project completed

--- index.js
+++ index.js
@@ -15,32 +15,54 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const productCollection = client.db(process.env.DB_NAME).collection("products");
-    
-    app.post("/addProducts", (req, res) => {
-      const newProduct= req.body;
-      console.log("New Event", newProduct);
-      productCollection.insertOne(newProduct)
+  const productCollection = client.db(process.env.DB_NAME).collection("products");
+
+  app.post("/addProducts", (req, res) => {
+    const newProduct = req.body;
+    console.log("New Event", newProduct);
+    productCollection.insertOne(newProduct)
       .then(result => {
         console.log("inserted count", result.insertedCount);
         res.send(result.insertedCount > 0)
       })
-    })
+  })
 
-    app.get('/products', (req, res) => {
-      productCollection.find()
+  app.get('/products', (req, res) => {
+    productCollection.find()
       .toArray((err, items) => {
         res.send(items);
       })
-    })
+  })
 
-    app.delete('/deleteProduct/:id', (req, res) => {
-      const id = ObjectID(req.params.id);
-      console.log('delete event', id);
-      productCollection.findOneAndDelete({_id: id})
+  app.delete('/deleteProduct/:id', (req, res) => {
+    const id = ObjectID(req.params.id);
+    console.log('delete event', id);
+    productCollection.findOneAndDelete({ _id: id })
       .then(documents => res.send(!!documents.value))
+  })
+
+});
+
+// Add Orders
+client.connect(err => {
+  const orderCollection = client.db(process.env.DB_NAME).collection("orders");
+
+  app.post('/addOrders', (req, res) => {
+    const newOrder = req.body;
+    orderCollection.insertOne(newOrder)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+  })
+
+  app.get('/orders', (req, res) => {
+    //console.log(req.query.email)
+    orderCollection.find({email: req.query.email})
+    .toArray((err, documents) => {
+      res.send(documents)
     })
-  
+  })
+
 });
 
 
